37253f53a502611a5b69cfc74e3f24aaf7268255
Mervyn Lam <mervyn1994@gmail.com>
2020-08-03 05:19:49 +0800

    **Optimizations** 1. Cache and reuse thumbnails, with trimmed Bookmark-url as key. Reduce no. of JSON requests to make. 2. Doesn't process needlessly when address field's text are the same. Also handled for multiple bookmark elements present. 3. Experimental: Support for Youtube API Key. (Used to retrieve thumbs for channel / playlist)

--- README.md
+++ README.md
@@ -1,57 +1,78 @@
-### Vivaldi Bookmark Thumbnail Adapter
+## Vivaldi Bookmark Thumbnail Adapter
 *Edited title. Initially called "Bookmark Thumbnail Hijacking - Workaround"*
 
 A workaround mod aimed to adapt bookmark thumbnails according to their URLs.
 Here's hoping that this mod becomes obsolete. (When thumbnails gets better support)
 
+![QueGER0KgW.gif](/assets/uploads/files/1596378734615-queger0kgw.gif) 
+
 #### Why?
-Initial intention is to cater thumbnail displays for youtube.
+The Initial intention was to cater thumbnail displays for youtube.
 1. Bookmarking videos while you're traversing a playlist (say, your "Likes" to find notable videos you've liked) tend to generate inaccurate thumbnails.
 2. Refreshing bookmark thumbnails leaves you with a start-of-video black screen which can be undesirable.
 3. Manually setting custom thumbnails for bulks of youtube bookmark isn't scaleable (takes up space on computer, and your effort/time)
 
-After some thought, I realize I could extend this to cover more Platforms / APIs other than Youtube.
+After some thought, I realize I could extend this to cover more Platforms / APIs other than Youtube. I'm doing this for learning and hobby too.
 
 ---
+
+#### Latest changes
+**Optimizations**
+1. Cache and reuse thumbnails, with trimmed Bookmark-url as key. Reduce no. of JSON requests to make.
+2. Doesn't process needlessly when address field's text are the same. Also handled for multiple bookmark elements present.
+3. Experimental: Support for Youtube API Key. (Used to retrieve thumbs for channel / playlist)
 
 #### Installation
 Follow the instructions at https://forum.vivaldi.net/topic/10549/modding-vivaldi
 
 #### Configuring
-In bookmarkThumbnailAdjustment.js,
+In `bookmarkThumbnailAdjustment.js`,
 You can increase the rate of update by lowering the 1000 (milliseconds) in
 ```
-var bookmarkThumbnailHijackerUpdateLoop = setInterval(hijackBookmarkThumbnail, 1000); 
+var bookmarkThumbnailAdapter = setInterval(adaptBookmarkThumbnails, 1000);
 ```
 
-In bookmarkThumbnailAdjustment.css,
-You can tweak its `object-fit` to `cover`, `contain`, `fill`.
-Refer to https://www.w3schools.com/csS/css3_object-fit.asp to see what each style does.
+Under the processDeviantArt function, you can reorder which image-type to you prefer.
+It'll attempt to grab the first available image-url it encounters.
+```
+testImage(url, userUrl+'.gif', success, ()=> {
+	testImage(url, userUrl+'.png', success, ()=> {
+		testImage(url, userUrl+'.jpg', success, ()=> {
+			//unhandled case, exhausted all known types
+		});
+	});
+});
+```
+
+In `bookmarkThumbnailAdjustment.css`,
+I've set `object-fit` to use `contain`, but you could change it `cover` or `fill` if it better fits your need.
 
 ### Currently Supports
-- Youtube (videos only; fallback to youtube's default if unavailable)
+- Youtube (videos only; channel and playlist thumbnails requires API Key)
 - Vimeo
 - Instagram (posts only)
 - deviantArt (profile page, with `oembed` fallbacks.)
 - Newgrounds (`portal/view` & `audio/listen` submissions only)
 
-For now, I'll be avoiding sites that requires authorizations. Generally struggling with fetching user avatars. There's bound to be bugs. Going to iron out the edge cases that I can over time.
+Going to iron out the edge cases that over time *if I can*.
 
-### Help needed
-1. Instead of updating by interval, can it be optimized to only update upon  
-    user interaction with said bookmark manager/sidebar?
-2. Report edge-cases or bugs, or share your fixes for them.
-3. Share ideas for other APIs that you think this could be used for.
+---
 
 #### References
+- [StackOverflow - Get DOM Path](https://stackoverflow.com/questions/12644147/getting-element-path-for-selector#answer-22072325)
 - [deponeWD video](https://github.com/deponeWD/video)
+- [Youtube Data API v3](https://developers.google.com/youtube/v3/docs/playlists/list)
 - [DeviantArt API, oembed specs](https://www.deviantart.com/developers/oembed#depths)
 - [GIPHY API Profile: Make Your Project More Fun with GIFs](https://rapidapi.com/blog/giphy-api-profile-make-your-project-more-fun-with-gifs/)
 - [GIPHY API Endpoints](https://developers.giphy.com/docs/api/endpoint#search)
-- [Newgrounds.io](https://www.newgrounds.io)
 - [Instagram fb Dev page](https://developers.facebook.com/docs/instagram)
+- [Newgrounds.io](https://www.newgrounds.io)
 
 #### Credits
-- deponeWD (support for multiple variations of youtube & vimeo)
-- LomN *for inspiration and moddng reference*
+- deponeWD (support for various types of youtube & vimeo urls)
+- sshow & jcern (for the DOM path function)
+- LomN (for the inspiration and moddng references)
 - Vivaldi modding community
+
+#### Other Notes
+The older versions and references I used are on this [Github page](https://github.com/azrav/vivaldiBookmarkThumbnailAdapter/commits/master)
--- bookmarkThumbnailAdapter.js
+++ bookmarkThumbnailAdapter.js
@@ -1,5 +1,25 @@
 /* Bookmark Thumbnail Adapter */
 
+var thumbDB = {};           //Thumbnail database to store images
+var addrFields = {};        //Map of address field element(s) using their DOMPaths as key
+var cyclesTillRefresh = 10; //No. of cycles till Data update
+var cycles = 10;            //No. of cycles counting
+var uID = 0;                //Unique ID counter for tracking address field elements
+
+//API Keys
+//---
+//  Self-insert for your Youtube API_KEY for your own use.
+//  I'm still trying to figure the quota for this.
+var youtubeAPIKey = '{YOUTUBE_API_KEY}';
+var giphyPublicBetaKey = 'dc6zaTOxFJmzC';   //yay
+
+//Set image source, and cache it with trimmed bookmarkUrl
+function setImgSrc(img, srcUrl, bookmarkUrl) {
+    img.src = srcUrl;
+    thumbDB[bookmarkUrl] = srcUrl;
+    console.log('cached ',bookmarkUrl,'->',srcUrl)
+}
+
 //JSON helper
 function getJSON(url, callback)
 {
@@ -21,24 +41,93 @@ function getJSON(url, callback)
 }
 
 //Image helper
-function testImage(url, found, fallback) {
+function testImage(bookmarkUrl, url, found, fallback) {
     var tester=new Image();
     tester.onload = ()=>{
-        found(tester);
+        found(bookmarkUrl, tester);
     };
     tester.onerror=fallback;
     tester.src=url;
 }
 
+//DOMPath helper
+//  https://stackoverflow.com/questions/12644147/getting-element-path-for-selector#answer-22072325
+function dompath(element) {
+    var path = '',
+    i, innerText, tag, selector, classes;
+
+    for (i = 0; element && element.nodeType == 1; element = element.parentNode, i++) {
+        innerText = element.childNodes.length === 0 ? element.innerHTML : '';
+        tag = element.tagName.toLowerCase();
+        classes = element.className;
+
+        // Skip <html> and <body> tags
+        if (tag === "html" || tag === "body")
+            continue;
+
+        if (element.id !== '') {
+            // If element has an ID, use only the ID of the element
+            selector = '#' + element.id;
+
+            // To use this with jQuery, return a path once we have an ID
+            // as it's no need to look for more parents afterwards.
+            //return selector + ' ' + path;
+        } else if (classes.length > 0) {
+            // If element has classes, use the element tag with the class names appended
+            selector = tag + '.' + classes.replace(/ /g , ".");
+        } else {
+            // If element has neither, print tag with containing text appended (if any)
+            selector = tag + ((innerText.length > 0) ? ":contains('" + innerText + "')" : "");
+        }
+
+        path = ' ' + selector + path;
+    }
+    return path;
+}
+
+//Youtube-Vimeo code logic taken from
+//  https://github.com/deponeWD/video
 function processYoutubeVimeo(img, url)
 {
-    //Regex taken from https://github.com/deponeWD/video
     let matches = url.match(/(http:|https:|)\/\/(player.|www.|m.)?(vimeo\.com|youtu(be\.com|\.be|be\.googleapis\.com))\/(video\/|embed\/|watch\?v=|v\/)?([A-Za-z0-9._%-]*)(\&\S+)?/);
     if (matches && matches.length > 6)
     {
         let vID = matches[6];
-        if (matches[3].indexOf('youtu') > -1) {
-            img.src = 'https://img.youtube.com/vi/' + vID + '/maxresdefault.jpg';
+        if (matches[3].indexOf('youtu') > -1)
+        {
+            //Check if this is a playlist
+            matches = url.match(/^.*playlist?list=([A-Za-z0-9._%-]*)(\&\S+)?$/)
+            if (matches) {
+                let playlistID = matches[1];
+
+                // https://developers.google.com/youtube/v3/docs/playlists/list
+                getJSON('https://www.googleapis.com/youtube/v3/playlists?part=snippet&id=' + playlistID +'&key=' + youtubeAPIKey,
+                (err, data) => {
+                    let parsedData = JSON.parse(data);
+                    let thumb = parsedData.items[0].snippet.thumbnails.default.url;
+                    setImgSrc(img, thumb, url);
+                });
+                return true;
+            }
+
+            //Check if this is a channel
+            matches = url.match(/^.*channel\/([A-Za-z0-9._%-]*)(\&\S+)?$/)
+            if (matches) {
+                let channelID = matches[1];
+
+                // https://developers.google.com/youtube/v3/docs/channels/list
+                getJSON('https://www.googleapis.com/youtube/v3/channels?part=snippet&id=' + channelID + '&key=' + youtubeAPIKey,
+                (err, data) => {
+                    let parsedData = JSON.parse(data);
+                    let thumb = parsedData.items[0].snippet.thumbnails.default.url;
+                    setImgSrc(img, thumb, url);
+                });
+                return true;
+            }
+
+            //Otherwise, assume that it's a video url
+            let thumb = 'https://img.youtube.com/vi/' + vID + '/maxresdefault.jpg';
+            setImgSrc(img, thumb, url);
         }
         else if (matches[3].indexOf('vimeo') > -1) {
             getJSON('https://vimeo.com/api/v2/video/' + vID + '.json',
@@ -50,7 +139,7 @@ function processYoutubeVimeo(img, url)
                 let thumbSplit = thumbSRClarge.split(/\d{3}(?=.jpg)/);
                 // add 1280x720 to parts and get bigger thumbnail
                 thumb = thumbSplit[0] + '1280x720' + thumbSplit[1];
-                img.src = thumb;
+                setImgSrc(img, thumb, url);
             });
         }
         return true;
@@ -65,7 +154,9 @@ function processInsta(img, url)
     {
         //Is a post
         if (url.indexOf('/p/') > -1) {
-            img.src = url + "/media";
+            // https://developers.facebook.com/docs/instagram/embedding
+            let thumb = url + "/media";
+            setImgSrc(img, thumb, url);
         }
         //todo: Else, its a username
         return true;
@@ -79,12 +170,13 @@ function processGiphy(img, url)
     if (matches != null)
     {
         let user = matches[1];
-        publicBetaKey = 'dc6zaTOxFJmzC';
-        getJSON('https://api.giphy.com/v1/gifs/search?q=' + user + '&limit=1&api_key='+publicBetaKey,
+
+        // https://developers.giphy.com/docs/api/endpoint#search
+        getJSON('https://api.giphy.com/v1/gifs/search?q=' + user + '&limit=1&api_key=' + giphyPublicBetaKey,
         (err, data) => {
             let parsedData = JSON.parse(data);
-            console.log(parsedData);
-            img.src = parsedData.data[0].user.avatar_url;
+            let thumb = parsedData.data[0].user.avatar_url;
+            setImgSrc(img, thumb, url);
         });
         return true;
     }
@@ -94,7 +186,8 @@ function processGiphy(img, url)
     {
         let trail = matches[1];
         let id = trail.substr(trail.lastIndexOf('-')+1);
-        img.src = 'https://media3.giphy.com/media/' + id + '/giphy.gif';
+        let thumb = 'https://media3.giphy.com/media/' + id + '/giphy.gif';
+        setImgSrc(img, thumb, url);
         return true;
     }
     return false;
@@ -114,12 +207,15 @@ function processDeviantArt(img, url)
         var userUrl = 'https://a.deviantart.net/avatars-big/' + user[0] +'/' + user[1] + '/' + user;
 
         //Success callback
-        let success = (foundImage)=>{
-            img.src = foundImage.src;
+        let success = (bookmarkUrl, foundImage)=>{
+            let thumb = foundImage.src;
+            setImgSrc(img, thumb, bookmarkUrl);
         }
-        testImage(userUrl+'.gif', success, ()=> {
-            testImage(userUrl+'.png', success, ()=> {
-                testImage(userUrl+'.jpg', success, ()=> {
+
+        //Note: You can reorder which image-type to grab first
+        testImage(url, userUrl+'.gif', success, ()=> {
+            testImage(url, userUrl+'.png', success, ()=> {
+                testImage(url, userUrl+'.jpg', success, ()=> {
                     //unhandled case, exhausted all known types
                 });
             });
@@ -127,14 +223,16 @@ function processDeviantArt(img, url)
         return true;
     }
 
-    //Fallback to oembed
+    //Fallback to oembed method
     matches = url.match(/^.*(deviantart\.com\/[A-Za-z0-9_-]+\/(art\/[A-Za-z0-9_-]+|.*#\/\d*)|(fav\.me\/[A-Za-z0-9_-]+|sta\.sh\/[A-Za-z0-9_-]+))$/);
     if (matches)
     {   
+        // https://www.deviantart.com/developers/oembed#depths
         getJSON('https://backend.deviantart.com/oembed?url=' + url,
         (err, data) => {
             let parsedData = JSON.parse(data);
-            img.src = parsedData.thumbnail_url;
+            let thumb = parsedData.thumbnail_url;
+            setImgSrc(img, thumb, url);
         });
         return true;
     }
@@ -151,12 +249,14 @@ function processNewgrounds(img, url)
         if (type == 'portal/view') {
             let culled = parseInt(id);
             culled = '' + Math.floor(culled / 1000) + '000';
-            img.src = 'https://picon.ngfiles.com/' + culled + '/flash_' + id + '.png';
+            let thumb = 'https://picon.ngfiles.com/' + culled + '/flash_' + id + '.png';
+            setImgSrc(img, thumb, url);
         }
         else if (type == 'audio/listen') {
             let culled = parseInt(id);
             culled = '' + Math.floor(culled / 1000);
-            img.src = 'https://aicon.ngfiles.com/' + culled + '/' + id + '.png';
+            let thumb = 'https://aicon.ngfiles.com/' + culled + '/' + id + '.png';
+            setImgSrc(img, thumb, url);
         }
         //other format WIP
         return true;
@@ -164,7 +264,24 @@ function processNewgrounds(img, url)
     return false;
 }
 
+//Process url to populate desired thumbnail
 function parseUrl (img, url) {
+    if (url == '')
+        return;
+
+    //Trim backslash
+    while (url.endsWith('/'))
+        url = url.slice(0,-1);
+
+    //If url was cached, pull a cached image-url
+    if (thumbDB.hasOwnProperty(url)) {
+        img.src = thumbDB[url];
+        return;
+    }
+    // console.log('parseUrl process', url);
+
+    //Process unrecognized url
+    //  leave if operation is done or delegated
     if (processYoutubeVimeo(img,url))   return;
     if (processInsta(img, url))         return;
     if (processGiphy(img, url))         return;
@@ -172,6 +289,7 @@ function parseUrl (img, url) {
     if (processNewgrounds(img, url))    return;
 }
 
+//Main update loop
 function adaptBookmarkThumbnails()
 {
     //Get list of bookmark cardviews
@@ -186,12 +304,60 @@ function adaptBookmarkThumbnails()
         //If both address and preview image elements are present
         if (urlField != null && img != null)
         {
-            let url = urlField.value;
-            while (url.endsWith('/'))
+            var url = urlField.value;
+            while (url.endsWith('/'))   //trim backslash
                 url = url.slice(0, -1);
+
+            let fieldPath = dompath(urlField);
+
+            //Is this field being tracked?
+            if (addrFields.hasOwnProperty(fieldPath))
+            {
+                //Element present, persist its life
+                addrFields[fieldPath].alive = true;
+
+                //Same address from before, leave
+                if (addrFields[fieldPath].last == url)
+                    return;
+            }
+            //No, track it
+            else {
+                addrFields[fieldPath] = {
+                    element: urlField,
+                    alive: true,
+                    track: ++uID //Increment unique-ID counter
+                };
+                // console.log('track', fieldPath);
+            }
+
+            //Set alive (as in present)
+            addrFields[fieldPath].last = urlField.value;
+
+            //Do the thing
             parseUrl(img, url);
         }
     });
+
+    //Data update
+    //  When this refresh is called, it clears all dead address-element references.
+    //  Each reference is used to track for address text value difference when they were alive.
+    if (++cycles > cyclesTillRefresh) {
+        cycles = 0;
+
+        //Gather expired fields to remove
+        let toRemove = []
+        for (let key of Object.keys(addrFields)) {
+            if (!addrFields[key].alive)
+                toRemove.push(key);
+            addrFields[key].alive = false;
+        }
+        //Stop tracking expired fields
+        toRemove.forEach((key, index)=>{
+            delete addrFields[key];
+            // console.log('untrack', key);
+        })
+    }
 }
 
+//Run update loop
 var bookmarkThumbnailAdapter = setInterval(adaptBookmarkThumbnails, 1000);
