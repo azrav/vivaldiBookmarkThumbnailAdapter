8a83a9ad7a6b63e4f6ef30b6297a6cc2a418204b
Mervyn Lam <mervyn1994@gmail.com>
2020-08-02 21:49:39 +0800

    Attempt at supporting a few more other sites's thumbnails - Youtube (videos only; fallback to youtube's default if unavailable) - Vimeo (haven't tested extensively) - Instagram (posts only) - deviantArt (profile page, with `oembed` fallback) - Newgrounds (`portal/view` & `audio/listen` submissions only)

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
-# Vivaldi Bookmark Thumbnail Adapter
-A workaround mod aimed to adapt bookmark thumbnails according to their URLs.
+### Vivaldi Bookmark Thumbnail Adapter
+*Edited title. Initially called "Bookmark Thumbnail Hijacking - Workaround"*
 
+A workaround mod aimed to adapt bookmark thumbnails according to their URLs.
 Here's hoping that this mod becomes obsolete. (When thumbnails gets better support)
 
-## Why?
-To satisfy my OCD.
-
+#### Why?
 Initial intention is to cater thumbnail displays for youtube.
 1. Bookmarking videos while you're traversing a playlist (say, your "Likes" to find notable videos you've liked) tend to generate inaccurate thumbnails.
 2. Refreshing bookmark thumbnails leaves you with a start-of-video black screen which can be undesirable.
 3. Manually setting custom thumbnails for bulks of youtube bookmark isn't scaleable (takes up space on computer, and your effort/time)
 
-After some thought, I realize I could perhaps extend this to cover more Platforms / APIs other than Youtube.
+After some thought, I realize I could extend this to cover more Platforms / APIs other than Youtube.
 
 ---
 
-## Installation
+#### Installation
 Follow the instructions at https://forum.vivaldi.net/topic/10549/modding-vivaldi
 
-### Configuring
+#### Configuring
 In bookmarkThumbnailAdjustment.js,
 You can increase the rate of update by lowering the 1000 (milliseconds) in
 ```
@@ -29,18 +28,30 @@ In bookmarkThumbnailAdjustment.css,
 You can tweak its `object-fit` to `cover`, `contain`, `fill`.
 Refer to https://www.w3schools.com/csS/css3_object-fit.asp to see what each style does.
 
-## Currently Supports
-### Youtube
-`https://www.youtube.com/watch?v=`**videoID**
-Some videos doesn't have thumbnails and will display Youtube's fallback thumbnail.
+### Currently Supports
+- Youtube (videos only; fallback to youtube's default if unavailable)
+- Vimeo
+- Instagram (posts only)
+- deviantArt (profile page, with `oembed` fallbacks.)
+- Newgrounds (`portal/view` & `audio/listen` submissions only)
 
-## Things I hope to support
-No guarantees.
-- DeviantArt
-- Instagram
-- Giphy
-- Pixiv
+For now, I'll be avoiding sites that requires authorizations. Generally struggling with fetching user avatars. There's bound to be bugs. Going to iron out the edge cases that I can over time.
 
-## Credits to
+### Help needed
+1. Instead of updating by interval, can it be optimized to only update upon  
+    user interaction with said bookmark manager/sidebar?
+2. Report edge-cases or bugs, or share your fixes for them.
+3. Share ideas for other APIs that you think this could be used for.
+
+#### References
+- [deponeWD video](https://github.com/deponeWD/video)
+- [DeviantArt API, oembed specs](https://www.deviantart.com/developers/oembed#depths)
+- [GIPHY API Profile: Make Your Project More Fun with GIFs](https://rapidapi.com/blog/giphy-api-profile-make-your-project-more-fun-with-gifs/)
+- [GIPHY API Endpoints](https://developers.giphy.com/docs/api/endpoint#search)
+- [Newgrounds.io](https://www.newgrounds.io)
+- [Instagram fb Dev page](https://developers.facebook.com/docs/instagram)
+
+#### Credits
+- deponeWD (support for multiple variations of youtube & vimeo)
 - LomN *for inspiration and moddng reference*
 - Vivaldi modding community
--- bookmarkThumbnailAdapter.js
+++ bookmarkThumbnailAdapter.js
@@ -1,8 +1,179 @@
-/* Bookmark Preview Hijack */
+/* Bookmark Thumbnail Adapter */
 
-const regexYoutubeWatch = /^.*youtube\.com\/watch\?v=([A-Za-z0-9_]+).*?$/g;
+//JSON helper
+function getJSON(url, callback)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onload = ()=> {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                callback(null, xhr.response);
+            } else {
+                console.error(xhr.statusText);
+            }
+        }
+    };
+    xhr.onerror = function (e) {
+        console.error(xhr.statusText);
+    };
+    xhr.send(null);
+}
 
-function hijackBookmarkThumbnail() {
+//Image helper
+function testImage(url, found, fallback) {
+    var tester=new Image();
+    tester.onload = ()=>{
+        found(tester);
+    };
+    tester.onerror=fallback;
+    tester.src=url;
+}
+
+function processYoutubeVimeo(img, url)
+{
+    //Regex taken from https://github.com/deponeWD/video
+    let matches = url.match(/(http:|https:|)\/\/(player.|www.|m.)?(vimeo\.com|youtu(be\.com|\.be|be\.googleapis\.com))\/(video\/|embed\/|watch\?v=|v\/)?([A-Za-z0-9._%-]*)(\&\S+)?/);
+    if (matches && matches.length > 6)
+    {
+        let vID = matches[6];
+        if (matches[3].indexOf('youtu') > -1) {
+            img.src = 'https://img.youtube.com/vi/' + vID + '/maxresdefault.jpg';
+        }
+        else if (matches[3].indexOf('vimeo') > -1) {
+            getJSON('https://vimeo.com/api/v2/video/' + vID + '.json',
+            (err, data) => {
+                let parsedData = JSON.parse(data);
+                let thumbSRClarge = parsedData[0].thumbnail_large;
+
+                // split url of large thumbnail at 640
+                let thumbSplit = thumbSRClarge.split(/\d{3}(?=.jpg)/);
+                // add 1280x720 to parts and get bigger thumbnail
+                thumb = thumbSplit[0] + '1280x720' + thumbSplit[1];
+                img.src = thumb;
+            });
+        }
+        return true;
+    }
+    return false;
+}
+
+function processInsta(img, url)
+{
+    matches = url.match(/^(http:|https:|).*\.instagram\.com\/p\/[A-Za-z0-9]+$/);
+    if (matches)
+    {
+        //Is a post
+        if (url.indexOf('/p/') > -1) {
+            img.src = url + "/media";
+        }
+        //todo: Else, its a username
+        return true;
+    }
+    return false;
+}
+
+function processGiphy(img, url)
+{
+    let matches = url.match(/giphy.com\/([A-Za-z0-9_-]+)$/);
+    if (matches != null)
+    {
+        let user = matches[1];
+        publicBetaKey = 'dc6zaTOxFJmzC';
+        getJSON('https://api.giphy.com/v1/gifs/search?q=' + user + '&limit=1&api_key='+publicBetaKey,
+        (err, data) => {
+            let parsedData = JSON.parse(data);
+            console.log(parsedData);
+            img.src = parsedData.data[0].user.avatar_url;
+        });
+        return true;
+    }
+
+    matches = url.match(/giphy.com\/gifs\/([A-Za-z0-9_-]+)$/);
+    if (matches != null)
+    {
+        let trail = matches[1];
+        let id = trail.substr(trail.lastIndexOf('-')+1);
+        img.src = 'https://media3.giphy.com/media/' + id + '/giphy.gif';
+        return true;
+    }
+    return false;
+}
+
+function processDeviantArt(img, url)
+{
+    let matches = url.match(/^.*deviantart\.com\/([A-Za-z0-9_-]+)$/);
+
+    //Handle deviantArt userpage
+    if (matches)
+    {
+        let user = matches[1];
+
+        //DeviantArt backend API has a /user/whois endpoint, but it requires authorization
+        //So I'm resorting to brute forcing image type
+        var userUrl = 'https://a.deviantart.net/avatars-big/' + user[0] +'/' + user[1] + '/' + user;
+
+        //Success callback
+        let success = (foundImage)=>{
+            img.src = foundImage.src;
+        }
+        testImage(userUrl+'.gif', success, ()=> {
+            testImage(userUrl+'.png', success, ()=> {
+                testImage(userUrl+'.jpg', success, ()=> {
+                    //unhandled case, exhausted all known types
+                });
+            });
+        });
+        return true;
+    }
+
+    //Fallback to oembed
+    matches = url.match(/^.*(deviantart\.com\/[A-Za-z0-9_-]+\/(art\/[A-Za-z0-9_-]+|.*#\/\d*)|(fav\.me\/[A-Za-z0-9_-]+|sta\.sh\/[A-Za-z0-9_-]+))$/);
+    if (matches)
+    {   
+        getJSON('https://backend.deviantart.com/oembed?url=' + url,
+        (err, data) => {
+            let parsedData = JSON.parse(data);
+            img.src = parsedData.thumbnail_url;
+        });
+        return true;
+    }
+    return false;
+}
+
+function processNewgrounds(img, url)
+{
+    let matches = url.match(/newgrounds.com\/(portal\/view|audio\/listen)\/(\d+)$/);
+    if (matches)
+    {
+        let type = matches[1];
+        let id = matches[2];
+        if (type == 'portal/view') {
+            let culled = parseInt(id);
+            culled = '' + Math.floor(culled / 1000) + '000';
+            img.src = 'https://picon.ngfiles.com/' + culled + '/flash_' + id + '.png';
+        }
+        else if (type == 'audio/listen') {
+            let culled = parseInt(id);
+            culled = '' + Math.floor(culled / 1000);
+            img.src = 'https://aicon.ngfiles.com/' + culled + '/' + id + '.png';
+        }
+        //other format WIP
+        return true;
+    }
+    return false;
+}
+
+function parseUrl (img, url) {
+    if (processYoutubeVimeo(img,url))   return;
+    if (processInsta(img, url))         return;
+    if (processGiphy(img, url))         return;
+    if (processDeviantArt(img, url))    return;
+    if (processNewgrounds(img, url))    return;
+}
+
+function adaptBookmarkThumbnails()
+{
     //Get list of bookmark cardviews
     let view = document.querySelectorAll('.addbookmark-cardwrapper .upper-half');
 
@@ -10,21 +181,17 @@ function hijackBookmarkThumbnail() {
     view.forEach(el => {
         //Fetch a list of its UrlInputFields & PreviewImages
         let urlField = el.querySelector('.title-and-address .fieldset .UrlField input[placeholder="Address"]');
-        let img = el.querySelector('.preview .thumbnail-image img');
+        var img = el.querySelector('.preview .thumbnail-image img');
 
         //If both address and preview image elements are present
         if (urlField != null && img != null)
         {
             let url = urlField.value;
-
-            //Replace youtube video ID if matched
-            const ytVidID = url.replace(regexYoutubeWatch, '$1');
-
-            //If replace is successful, set youtube video thumbnail using ID
-            if (url != ytVidID)
-                img.src = "https://img.youtube.com/vi/" + ytVidID + "/maxresdefault.jpg";
+            while (url.endsWith('/'))
+                url = url.slice(0, -1);
+            parseUrl(img, url);
         }
     });
 }
 
-var bookmarkThumbnailHijackerLoop = setInterval(hijackBookmarkThumbnail, 1000); 
+var bookmarkThumbnailAdapter = setInterval(adaptBookmarkThumbnails, 1000);
